e04ec51d016e01a82d393f48229681f6d1b60dbc
Clemens Lutz <lutzcle@cml.li>
2016-01-28 19:11:34 +0100

    Simplify OpenCL initializer class by replacing pointers with actual values (who cares about some extra copies?)

--- initializer.cpp
+++ initializer.cpp
@@ -12,31 +12,6 @@
 #include <CL/cl.hpp>
 #endif
 
-cle::CLInitializer::CLInitializer() :
-    platform_(NULL),
-    context_(NULL),
-    device_(NULL),
-    queue_(NULL)
-{}
-
-cle::CLInitializer::~CLInitializer() {
-    if (this->context_ != NULL) {
-        delete this->context_;
-    }
-
-    if (this->device_ != NULL) {
-        delete this->device_;
-    }
-
-    if (this->platform_ != NULL) {
-        delete this->platform_;
-    }
-
-    if (this->queue_ != NULL) {
-        delete this->queue_;
-    }
-}
-
 
 int cle::CLInitializer::choose_platform_interactive() {
     cl_int err;
@@ -59,7 +34,7 @@ int cle::CLInitializer::choose_platform_interactive() {
     std::cout << "Enter platform number: " << std::flush;
     std::cin >> choice;
     std::cout << std::endl;
-    this->platform_ = new cl::Platform(platforms[choice]);
+    this->platform_ = cl::Platform(platforms[choice]);
 
     // Create context
     cl_context_properties cps[3] = {
@@ -68,7 +43,7 @@ int cle::CLInitializer::choose_platform_interactive() {
     };
 
     cle_sanitize_ref_return(
-            this->context_ = new cl::Context(
+            this->context_ = cl::Context(
                 CL_DEVICE_TYPE_ALL,
                 cps,
                 NULL,
@@ -81,22 +56,31 @@ int cle::CLInitializer::choose_platform_interactive() {
 
 int cle::CLInitializer::choose_device_interactive() {
     cl_int err;
+    cl_uint num_devices;
     std::vector<cl::Device> devices;
 
     cle_sanitize_val_return(
-            this->context_->getInfo(
+            this->context_.getInfo(
+                CL_CONTEXT_NUM_DEVICES,
+                &num_devices
+                ));
+
+    devices.resize(num_devices);
+
+    cle_sanitize_val_return(
+            this->context_.getInfo(
                 CL_CONTEXT_DEVICES,
                 &devices
                 ));
 
     // TODO: show devices, choose device
-    this->device_ = new cl::Device(devices[0]);
+    this->device_ = cl::Device(devices[0]);
 
     // Create command queue for device
     cle_sanitize_ref_return(
-            this->queue_ = new cl::CommandQueue(
-                *this->context_,
-                *this->device_,
+            this->queue_ = cl::CommandQueue(
+                this->context_,
+                this->device_,
                 CL_QUEUE_PROFILING_ENABLE,
                 &err
                 ), err);
@@ -104,18 +88,18 @@ int cle::CLInitializer::choose_device_interactive() {
     return 1;
 }
 
-cl::Platform& cle::CLInitializer::get_platform() {
-    return *this->platform_;
+cl::Platform cle::CLInitializer::get_platform() {
+    return this->platform_;
 }
 
-cl::Context& cle::CLInitializer::get_context() {
-    return *this->context_;
+cl::Context cle::CLInitializer::get_context() {
+    return this->context_;
 }
 
-cl::Device& cle::CLInitializer::get_device() {
-    return *this->device_;
+cl::Device cle::CLInitializer::get_device() {
+    return this->device_;
 }
 
-cl::CommandQueue& cle::CLInitializer::get_commandqueue() {
-    return *this->queue_;
+cl::CommandQueue cle::CLInitializer::get_commandqueue() {
+    return this->queue_;
 }
--- initializer.hpp
+++ initializer.hpp
@@ -10,22 +10,19 @@
 namespace cle {
     class CLInitializer {
     public:
-        CLInitializer();
-        ~CLInitializer();
-
         int choose_platform_interactive();
         int choose_device_interactive();
 
-        cl::Platform& get_platform();
-        cl::Context& get_context();
-        cl::Device& get_device();
-        cl::CommandQueue& get_commandqueue();
+        cl::Platform get_platform();
+        cl::Context get_context();
+        cl::Device get_device();
+        cl::CommandQueue get_commandqueue();
 
     private:
-        cl::Platform *platform_;
-        cl::Context *context_;
-        cl::Device *device_;
-        cl::CommandQueue *queue_;
+        cl::Platform platform_;
+        cl::Context context_;
+        cl::Device device_;
+        cl::CommandQueue queue_;
     };
 }
 

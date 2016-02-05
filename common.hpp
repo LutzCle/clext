ff5af70330822534d9cadc43e6d87901aa138b54
Clemens Lutz <lutzcle@cml.li>
2016-02-05 11:28:19 +0100

    License code under MPL 2.0 and add copyright notice

--- common.hpp
+++ common.hpp
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * 
+ * Copyright (c) 2016, Lutz, Clemens <lutzcle@cml.li>
+ */
+
 #ifndef CLE_COMMON_HPP
 #define CLE_COMMON_HPP
 

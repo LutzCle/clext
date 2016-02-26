13e6ab1b869deb7f8d5113b0bc6c4b70c0d18bd4
Clemens Lutz <lutzcle@cml.li>
2016-02-26 11:46:48 +0100

    Clean up and make the library buildable

--- kernel.hpp
+++ kernel.hpp
@@ -19,12 +19,6 @@
 #include <CL/cl.hpp>
 #endif
 
-#include "SystemConfig.h"
-
-
-// Append path prefix defined in CMakeLists to OpenCL kernel file name
-#define CL_KERNEL_FILE_PATH(FILE_NAME) CL_KERNELS_PATH "/" FILE_NAME
-
 namespace cle {
 
     cl_int show_platforms(std::vector<cl::Platform> const& platforms);
